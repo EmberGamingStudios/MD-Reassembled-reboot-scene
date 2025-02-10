b31678ac97d1c3e4c38badae3197a15e3e8e462b
EmberGamingStudios <157764375+EmberGamingStudios@users.noreply.github.com>
2025-02-10 15:05:36 -0600

    Add files via upload

---
+++ coming_soon.sh
@@ -0,0 +1,362 @@
+#!/bin/bash
+clear
+sleep 1
+
+
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████         ████████    ██████      ██████    ██      ██ "
+echo "                                       ██      ██  ██      ██  ████  ████  ██  ████    ██  ██             ██          ██      ██  ██      ██  ████    ██ "
+echo "                                       ██          ██      ██  ██  ██  ██  ██  ██  ██  ██  ██    ████       ██████    ██      ██  ██      ██  ██  ██  ██ "
+echo "                                       ██      ██  ██      ██  ██      ██  ██  ██    ████  ██      ██             ██  ██      ██  ██      ██  ██    ████ "
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████       ████████      ██████      ██████    ██      ██ "
+echo ""
+sleep 1
+echo "" #"                                                                                 █▀ ▄ ▄▄▄▄  ▄  ▄▄▄  ▄▄▄▄▄ ▀█ "
+echo "" #"                                                                                 █  █ █   █ █ █   █   █    █ "
+echo "" #"                                                                                 █  █ █▄▄▄▀ █ ▀▄▄▄▀   █    █ "
+echo "" #"                                                                                 ▀▀                       ▀▀ "
+
+
+
+
+
+sleep 0.5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clear
+echo ""
+echo "SERIAL DESIGNATION: N-OXOO1OO1O"
+echo "RUNNING DIAGNOSTICS..."
+echo "SUBSTITUTING MATERIAL FORM //PERMISSION DENIED//"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████         ████████    ██████      ██████    ██      ██ "
+echo "                                       ██      ██  ██      ██  ████  ████  ██  ████    ██  ██             ██          ██      ██  ██      ██  ████    ██ "
+echo "                                       ██          ██      ██  ██  ██  ██  ██  ██  ██  ██  ██    ████       ██████    ██      ██  ██      ██  ██  ██  ██ "
+echo "                                       ██      ██  ██      ██  ██      ██  ██  ██    ████  ██      ██             ██  ██      ██  ██      ██  ██    ████ "
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████       ████████      ██████      ██████    ██      ██ "
+echo ""
+echo "" # "                                                                                 █▀ ▄ ▄▄▄▄  ▄  ▄▄▄  ▄▄▄▄▄ ▀█ "
+echo "" # "                                                                                 █  █ █   █ █ █   █   █    █ "
+echo "" # "                                                                                 █  █ █▄▄▄▀ █ ▀▄▄▄▀   █    █ "
+echo "" # "                                                                                 ▀▀                       ▀▀ "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+term_width=$(tput cols)
+text0="WOULD YOU LIKE TO SEND AN ERROR REPORT"
+indent=$((term_width - ${#text0}))
+printf "%${indent}s%s\n" "" "$text0"
+
+
+
+sleep 0.5
+clear
+echo ""
+echo "SERIAL DESIGNATION: N-OXOO1OO1O"
+echo "COMPLETE"
+echo "SUBSTITUTING MATERIAL FORM //PERMISSION DENIED//"
+echo 'STRING "ABSOLUTE SOLVER" BLOCKED BY ADMINISTRATION "CYN"'
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████         ████████    ██████      ██████    ██      ██ "
+echo "                                       ██      ██  ██      ██  ████  ████  ██  ████    ██  ██             ██          ██      ██  ██      ██  ████    ██ "
+echo "                                       ██          ██      ██  ██  ██  ██  ██  ██  ██  ██  ██    ████       ██████    ██      ██  ██      ██  ██  ██  ██ "
+echo "                                       ██      ██  ██      ██  ██      ██  ██  ██    ████  ██      ██             ██  ██      ██  ██      ██  ██    ████ "
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████       ████████      ██████      ██████    ██      ██ "
+echo ""
+echo "" # "                                                                                 █▀ ▄ ▄▄▄▄  ▄  ▄▄▄  ▄▄▄▄▄ ▀█ "
+echo "" # "                                                                                 █  █ █   █ █ █   █   █    █ "
+echo "" # "                                                                                 █  █ █▄▄▄▀ █ ▀▄▄▄▀   █    █ "
+echo "" # "                                                                                 ▀▀                       ▀▀ "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+term_width=$(tput cols)
+text00="WOULD YOU LIKE TO SEND AN ERROR REPORT"
+indent=$((term_width - ${#text00}))
+printf "%${indent}s%s\n" "" "$text00"
+
+
+sleep 0.5
+clear
+echo ""
+echo "SERIAL DESIGNATION: N-OXOO1OO1O"
+echo "COMPLETE"
+echo "SUBSTITUTING MATERIAL FORM //PERMISSION DENIED//"
+echo 'STRING "ABSOLUTE SOLVER" BLOCKED BY ADMINISTRATION "CYN"'
+echo "RECOVERING FROM BACKUP, PLEASE WAIT..."
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████         ████████    ██████      ██████    ██      ██ "
+echo "                                       ██      ██  ██      ██  ████  ████  ██  ████    ██  ██             ██          ██      ██  ██      ██  ████    ██ "
+echo "                                       ██          ██      ██  ██  ██  ██  ██  ██  ██  ██  ██    ████       ██████    ██      ██  ██      ██  ██  ██  ██ "
+echo "                                       ██      ██  ██      ██  ██      ██  ██  ██    ████  ██      ██             ██  ██      ██  ██      ██  ██    ████ "
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████       ████████      ██████      ██████    ██      ██ "
+echo ""
+echo "" # "                                                                                 █▀ ▄ ▄▄▄▄  ▄  ▄▄▄  ▄▄▄▄▄ ▀█ "
+echo "" # "                                                                                 █  █ █   █ █ █   █   █    █ "
+echo "" # "                                                                                 █  █ █▄▄▄▀ █ ▀▄▄▄▀   █    █ "
+echo "" # "                                                                                 ▀▀                       ▀▀ "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+term_width=$(tput cols)
+text="WOULD YOU LIKE TO SEND AN ERROR REPORT"
+indent=$((term_width - ${#text}))
+printf "%${indent}s%s\n" "" "$text"
+
+
+
+
+sleep 0.5
+clear
+echo ""
+echo "SERIAL DESIGNATION: N-OXOO1OO1O"
+echo "COMPLETE"
+echo "DENIED [AUTO RUN COMPLETE]"
+echo '> STRING "ABSOLUTE SOLVER" BLOCKED BY ADMINISTRATION "CYN"'
+echo "REBOOTING, PLEASE WAIT..."
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████         ████████    ██████      ██████    ██      ██ "
+echo "                                       ██      ██  ██      ██  ████  ████  ██  ████    ██  ██             ██          ██      ██  ██      ██  ████    ██ "
+echo "                                       ██          ██      ██  ██  ██  ██  ██  ██  ██  ██  ██    ████       ██████    ██      ██  ██      ██  ██  ██  ██ "
+echo "                                       ██      ██  ██      ██  ██      ██  ██  ██    ████  ██      ██             ██  ██      ██  ██      ██  ██    ████ "
+echo "                                         ██████      ██████    ██      ██  ██  ██      ██    ██████       ████████      ██████      ██████    ██      ██ "
+echo ""
+echo "" # "                                                                                 █▀ ▄ ▄▄▄▄  ▄  ▄▄▄  ▄▄▄▄▄ ▀█ "
+echo "" # "                                                                                 █  █ █   █ █ █   █   █    █ "
+echo "" # "                                                                                 █  █ █▄▄▄▀ █ ▀▄▄▄▀   █    █ "
+echo "" # "                                                                                 ▀▀                       ▀▀ "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+term_width=$(tput cols)
+text3="DATA RESTORATION IN PROGRESS"
+indent=$((term_width - ${#text3}))
+printf "%${indent}s%s\n" "" "$text3"
+
+term_width=$(tput cols)
+text2="ENTERING SAFE MODE...SUCCESS"
+indent=$((term_width - ${#text2}))
+printf "%${indent}s%s\n" "" "$text2"
+
+sleep 0.2
+term_width=$(tput cols)
+text1="PLEASE SEE A CERTIFIED TECHNICIAN"
+indent=$((term_width - ${#text1}))
+printf "%${indent}s%s\n" "" "$text1"
+
+
+
+
+sleep 0.5
+clear
+echo ""
+echo "SERIAL DESIGNATION: N-OXOO1OO1O"
+echo "COMPLETE"
+echo "DENIED [AUTO RUN COMPLETE]"
+echo '> STRING "ABSOLUTE SOLVER" BLOCKED BY ADMINISTRATION "CYN"'
+echo "REBOOTING, PLEASE WAIT..."
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "                                                ██            ██████    ██████████  ██████████     ████████      ██████    ████████    ██████████ "
+echo "                                                ██          ██      ██      ██      ██                     ██  ██      ██          ██  ██         "
+echo "                                                ██          ██████████      ██      ████████         ██████    ██      ██    ██████    ████████   "
+echo "                                                ██          ██      ██      ██      ██             ██          ██      ██  ██                  ██ "
+echo "                                                ██████████  ██      ██      ██      ██████████     ██████████    ██████    ██████████  ████████   "
+echo ""
+echo "                                                                        "
+echo "                                                                        "
+echo "                                                                        "
+echo "                                                                        "
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+term_width=$(tput cols)
+text30="DATA RESTORATION IN PROGRESS"
+indent=$((term_width - ${#text30}))
+printf "%${indent}s%s\n" "" "$text30"
+
+term_width=$(tput cols)
+text20="ENTERING SAFE MODE...SUCCESS"
+indent=$((term_width - ${#text20}))
+printf "%${indent}s%s\n" "" "$text20"
+
+term_width=$(tput cols)
+text10="PLEASE SEE A CERTIFIED TECHNICIAN"
+indent=$((term_width - ${#text10}))
+printf "%${indent}s%s\n" "" "$text10"
+
+
+
+sleep 5
